2ff5bfb8a8963d4adf8948aabe3934a76b7b4087
iamtalibkhan <thetalibkhan555@gmail.com>
2021-11-10 13:52:19 +0530

    Talib

---
+++ zonbike/compo/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+
+    return `
+    <div id="options">
+        <h3><a href="whyon.html">WHY ON?</a></h3>
+        <h3><a href="fleet&pricing.html">FLEET AND PRICING</a></h3>
+        <h3><a href="safety.html">SAFETY</a></h3>
+        <h3><a href="#"><img src="https://www.onnbikes.com/img/ONN-logo-unit-without-shadow.png" alt=""></a></h3>
+        <h3><a href="faq.html">FAKE</a></h3>
+        <h3><a href="/signinsignup.html">SIGNIN & SIGNUP</a></h3>
+        <h3><a href="number.html">7498913829</a></h3>
+    </div>`
+}
+
+export default navbar
+
