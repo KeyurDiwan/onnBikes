5c3a7d29d14266d977930369795a776dee8c1f9b
keyur diwan <keyurdiwan009@gmail.com>
2021-11-12 01:22:53 +0530

    Add blog page!

---
+++ BLOGpage.js
@@ -0,0 +1,78 @@
+
+function background1() {
+    let myhover = document.getElementById("background1");
+    myhover.style.background = "aqua"
+}
+
+
+function background2() {
+    let myhover = document.getElementById("background2");
+    myhover.style.background = "aqua"
+}
+
+
+function background3() {
+    let myhover = document.getElementById("background3");
+    myhover.style.background = "aqua"
+}
+
+
+function background4() {
+    let myhover = document.getElementById("background4");
+    myhover.style.background = "aqua"
+}
+
+
+function background5() {
+    let myhover = document.getElementById("background5");
+    myhover.style.background = "aqua"
+}
+
+
+function background6() {
+    let myhover = document.getElementById("background6");
+    myhover.style.background = "aqua"
+}
+
+
+function background7() {
+    let myhover = document.getElementById("background7");
+    myhover.style.background = "aqua"
+}
+
+
+function background8() {
+    let myhover = document.getElementById("background8");
+    myhover.style.background = "aqua"
+}
+
+
+function background9() {
+    let myhover = document.getElementById("background9");
+    myhover.style.background = "aqua"
+}
+
+
+function background10() {
+    let myhover = document.getElementById("background10");
+    myhover.style.background = "aqua"
+}
+
+
+function background11() {
+    let myhover = document.getElementById("background11");
+    myhover.style.background = "aqua"
+}
+
+
+function background12() {
+    let myhover = document.getElementById("background12");
+    myhover.style.background = "aqua"
+}
+
+
+function sharenow() {
+    alert("Thank you for sharing.")
+}
+
+
